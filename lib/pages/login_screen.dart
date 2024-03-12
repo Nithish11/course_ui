3d2e4b1523466815bac3594a443f17dccf912225
Nithish11 <nithishusha31@gmail.com>
2024-03-12 23:53:07 +0530

    feat:added the login textfield

--- lib/pages/login_screen.dart
+++ lib/pages/login_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
+// import 'package:google_fonts/google_fonts.dart';
 
 class login_page extends StatefulWidget {
   const login_page({super.key});
@@ -9,62 +9,90 @@ class login_page extends StatefulWidget {
 }
 
 class _login_pageState extends State<login_page> {
-  
   @override
   Widget build(BuildContext context) {
     double screen_width = MediaQuery.of(context).size.width;
     double screen_height = MediaQuery.of(context).size.height;
+
     return Scaffold(
-      body: Column(
-        children: [
-          Container(
-            height: screen_height * 0.62,
-            width: screen_width * 1,
-            decoration: BoxDecoration(
-              image: DecorationImage(image: AssetImage("assets/02.png")),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: screen_height * 0.55,
+              width: screen_width * 1,
+              decoration: const BoxDecoration(
+                image: DecorationImage(image: AssetImage("assets/02.png")),
+              ),
             ),
-          ),
-          Text(
-            "Let's Start!",
-            style: TextStyle(
-                fontSize: 45,
-                fontWeight: FontWeight.w900,
-                color: Color.fromARGB(255, 6, 12, 73)),
-          ),
-          SizedBox(
-            height: 15,
-          ),
-          
-          SizedBox(
-            height: 100,
-          ),
-          startbutton(),
-          
-        ],
+            const Text(
+              "Let's Start!",
+              style: TextStyle(
+                  fontSize: 50,
+                  fontWeight: FontWeight.w900,
+                  color: Color.fromARGB(255, 6, 12, 73)),
+            ),
+            const Padding(
+              padding:  EdgeInsets.symmetric(horizontal: 22,vertical: 10),
+              child:  TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.black),
+                    ),
+                    labelText: "Email",
+                    hintText: "Enter valid mail id as abc@gmail.com",
+                ),
+              ),
+            ),
+            const Padding(
+              padding:  EdgeInsets.symmetric(horizontal: 22,vertical: 10),
+              child:  Row(
+                children: [
+                  TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.black),
+                        ),
+                        labelText: "Password",
+                        hintText: "Enter the alphabets with number, symbols",   
+                    ),
+                    obscureText: true,
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            
+            startbutton(),
+            
+          ],
+        ),
       ),
     );
   }
 
   ElevatedButton startbutton() {
     return ElevatedButton(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 17, vertical: 18),
-            child: Text(
-              'Start Now',
-              style: TextStyle(color: Colors.white),
-            ),
-          ),
-          style: ElevatedButton.styleFrom(
-            backgroundColor: Color.fromARGB(255, 12, 3, 40),
-            textStyle:
-                const TextStyle(fontSize: 24, fontStyle: FontStyle.normal),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(12.0),
-            ),
-          ),
-          onPressed: () {
-            Navigator.push(context,MaterialPageRoute(builder: (context) =>login_page()));
-          },
-        );
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 17, vertical: 18),
+        child: Text(
+          'Start Now',
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+      style: ElevatedButton.styleFrom(
+        backgroundColor: Color.fromARGB(255, 12, 3, 40),
+        textStyle: const TextStyle(fontSize: 24, fontStyle: FontStyle.normal),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(12.0),
+        ),
+      ),
+      onPressed: () {
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => login_page()));
+      },
+    );
   }
 }
--- lib/pages/start_screen.dart
+++ lib/pages/start_screen.dart
@@ -1,6 +1,6 @@
 import 'package:course_design/pages/login_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
+// import 'package:google_fonts/google_fonts.dart';
 
 class start_screen extends StatefulWidget {
   const start_screen({super.key});
@@ -16,55 +16,57 @@ class _start_screenState extends State<start_screen> {
     double screen_height = MediaQuery.of(context).size.height;
 
     return Scaffold(
-      body: Column(
-        children: [
-          Container(
-            height: screen_height * 0.70,
-            width: screen_width * 1,
-            decoration: BoxDecoration(
-              image: DecorationImage(image: AssetImage("assets/01.png")),
-              // color: Colors.red,
-            ),
-          ),
-          Text(
-            "Learn at home",
-            style: GoogleFonts.aBeeZee(
-                fontSize: 50,
-                fontWeight: FontWeight.w900,
-                color: Color.fromARGB(255, 6, 12, 73)),
-          ),
-          SizedBox(
-            height: 15,
-          ),
-          Text(
-            "efficiently and effectively",
-            style: TextStyle(
-                fontSize: 20, color: Color.fromARGB(255, 157, 165, 170)),
-          ),
-          SizedBox(
-            height: 62,
-          ),
-          ElevatedButton(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 17, vertical: 18),
-              child: Text(
-                'Start Now',
-                style: TextStyle(color: Colors.white),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: screen_height * 0.60,
+              width: screen_width * 1,
+              decoration: BoxDecoration(
+                image: DecorationImage(image: AssetImage("assets/01.png")),
+                // color: Colors.red,
               ),
             ),
-            style: ElevatedButton.styleFrom(
-              backgroundColor: Color.fromARGB(255, 12, 3, 40),
-              textStyle:
-                  const TextStyle(fontSize: 24, fontStyle: FontStyle.normal),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(12.0),
-              ),
+            Text(
+              "Learn at home",
+              style: TextStyle(
+                  fontSize: 50,
+                  fontWeight: FontWeight.w900,
+                  color: Color.fromARGB(255, 6, 12, 73)),
             ),
-            onPressed: () {
-              Navigator.push(context,MaterialPageRoute(builder: (context) =>login_page()));
-            },
-          ),
-        ],
+            SizedBox(
+              height: 15,
+            ),
+            Text(
+              "efficiently and effectively",
+              style: TextStyle(
+                  fontSize: 20, color: Color.fromARGB(255, 157, 165, 170)),
+            ),
+            SizedBox(
+              height: 67,
+            ),
+            ElevatedButton(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 17, vertical: 18),
+                child: Text(
+                  'Start Now',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Color.fromARGB(255, 12, 3, 40),
+                textStyle:
+                    const TextStyle(fontSize: 24, fontStyle: FontStyle.normal),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12.0),
+                ),
+              ),
+              onPressed: () {
+                Navigator.push(context,MaterialPageRoute(builder: (context) =>login_page()));
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
